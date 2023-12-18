515dd19ba859921619dba9e2e8e197c05bcc81e7
khaleelekan <kalilulekan@gmail.com>
2023-12-18 13:58:20 -0800

    new

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -15,12 +15,4 @@ def safe_print_integer(value):
         return True  # Return True if printing is successful
     except (TypeError, ValueError):
         return False  # Return False if a TypeError or ValueError occurs
-
-# Example usage:
-if __name__ == "__main__":
-    result1 = safe_print_integer(123)
-    print("Result 1:", result1)  # Output: 123 \n Result 1: True
-
-    result2 = safe_print_integer("abc")
-    print("Result 2:", result2)  # Output: Result 2: False
-
+    
