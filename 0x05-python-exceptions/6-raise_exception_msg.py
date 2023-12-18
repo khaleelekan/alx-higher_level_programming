cb8d62e536a4d5720cad67c726b9b8e6ae8b02c2
khaleelekan <kalilulekan@gmail.com>
2023-12-18 14:23:01 -0800

    new

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,12 @@
-g!/usr/bin/python3
+#!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    """Raise a NameError exception with a message."""
+    """Raise a NameError with a specified message.
+
+    Args:
+        message (str): The message to include in the NameError.
+
+    Raises:
+        NameError: Always raises a NameError with the specified message.
+    """
     raise NameError(message)
